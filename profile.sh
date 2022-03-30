f90096a276f3d6ec0df847ba9f4d93fef73531af
elsangithub <102652357+elsangithub@users.noreply.github.com>
2022-03-31 00:44:06 +0900

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
 neofetch
-echo -e " Selamat datang di layanan AutoScript LingSSH"
+echo -e " Selamat datang di layanan AutoScript Snell Elsan"
 echo -e " Ketik \e[1;32mmenu\e[0m untuk menampilkan daftar perintah"
 echo -e ""
