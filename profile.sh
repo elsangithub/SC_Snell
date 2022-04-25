393aca1d2125df2b81c895d26a1c324992338bd9
elsangithub <102652357+elsangithub@users.noreply.github.com>
2022-04-25 13:37:47 +0900

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -2,5 +2,5 @@
 clear
 neofetch
 echo -e " Selamat datang di layanan AutoScript Snell Elsan"
-echo -e " Ketik \e[1;32mmenu\e[0m untuk menampilkan daftar perintah"
+echo -e " Ketik \e[1;32m nano /etc/snell-server.conf \e[0m untuk menampilkan ulang PORT dan PSK dan merubahnya"
 echo -e ""
